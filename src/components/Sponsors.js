980ab10badfebbb85ce5e4742d483d6355d2baae
amrmohamed-9 <amrm7md9@gmail.com>
2025-03-20 14:24:04 +0200

    إضافة آخر التعديلات وإصلاح بعض الأخطاء

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -2,7 +2,7 @@
 import './css/Sponsors.css'
 
 function Sponsors(){
-    const logoVoom = "https://res.cloudinary.com/dxoongqsw/image/upload/v1742392540/LOGO-WHITE_fyfhnh.png"
+    const logoVoom = "https://res.cloudinary.com/dxoongqsw/image/upload/v1742472626/logonew_vduwic.png"
     const logoLv = "https://res.cloudinary.com/dxoongqsw/image/upload/v1742392539/LV03_sdvuml.png"
     return(
         <section className='sponsors' id="sponsors">
@@ -10,11 +10,11 @@ function Sponsors(){
                 <div className='main-partner text-center'>
                     <h2 className='font-q'>Main Sponsor</h2>
                     <div className='sponsor-img'>
-                        <a href='https://voomglobal.com/' target='_blank' rel="noreferrer"><img src={logoVoom} width={400} alt='voom global' className='my-3' loading='lazy'/></a>
+                        <a href='https://voomglobal.com/' target='_blank' rel="noreferrer"><img src={logoVoom} width={210} alt='voom global' className='my-3' loading='lazy'/></a>
                     </div>
                     <h2 className='font-q mt-5'>Gold Sponsor</h2>
                     <div className='sponsor-img'>
-                    <a href='https://www.facebook.com/Leconcompany/' target='_blank' rel="noreferrer"><img src={logoLv}  alt='voom global' width={220} className='my-3' loading='lazy'/></a>
+                    <a href='https://www.facebook.com/Leconcompany/' target='_blank' rel="noreferrer"><img src={logoLv}  alt='voom global' width={170} className='my-3' loading='lazy'/></a>
                     </div>
                 </div>
             </div>
